258639c3e2c22a20ab2694c02760ce016d06c021
swing <bestswngs@gmail.com>
2022-06-06 01:02:46 +0800

    update: Optimize the build directory, and add new msl.py files.

--- README.md
+++ README.md
@@ -1,20 +1,19 @@
 # ctfpwn-env
-
-
-
 ## introduction
+A project that uses containers to manage linux pwn environments.
 
-A tool for managing glibc pwn environments using containers.
+## swpwn
+A tool for managing glibc pwn environments using containers. This is a simplified version of [ancypwn](https://github.com/Escapingbug/ancypwn)
 
 It uses docker to manage the tools you might need, so you have separete environment, and you do all the debugging with the shared folder.
 
-## swpwn
+
 ### install
 
 `python3 -m pip install swpwn` 
 
 ### usage
-This is a simplified version of [ancypwn](https://github.com/Escapingbug/ancypwn)
+
 Contains only the container management parts.
 - run     
   run a pwn environments using containers.  
@@ -29,6 +28,74 @@ Contains only the container management parts.
   stop and delete a running container.      
   usage:  `swpwn end [container-name]`  
 
+## MSL (MacOS subsystem Linux power by Docker)
+Inspired by WSL, running a persistent container on MacOS via Docker
+
+### usage
+```bash
+$ msl --help
+usage: msl [-h] [--ubuntu UBUNTU] [--name NAME] [--priv] [--dir DIRECTORY] [--run RUN [RUN ...]] [--commit] [--loglevel LOGLEVEL]
+
+MacOS subsystem Linux power by Docker
+
+optional arguments:
+  -h, --help           show this help message and exit
+  --ubuntu UBUNTU      choice ubuntu version
+  --name NAME          container name
+  --priv               privileged boot , so you can use something like kvm
+  --dir DIRECTORY      shared directory to be mounted at /workhub
+  --run RUN [RUN ...]  run command
+  --commit             commit container to image
+  --loglevel LOGLEVEL  log level
+
+```
+
+Run a shell in the current directory
+```bash
+# swing @ WorkMac in ~/Desktop/ctfpwn-env on git:main x [0:52:28]
+$ msl
+[check_container() - msl:93 ] Container status: running
+[main() - msl:139 ] Running command: bash -c "cd '/workhub/Desktop/ctfpwn-env' ; zsh"
+
+__________               .__  .__  _____
+\______   \__  _  ______ |  | |__|/ ____\____
+ |     ___/\ \/ \/ /    \|  | |  \   __\/ __ \
+ |    |     \     /   |  \  |_|  ||  | \  ___/
+ |____|      \/\_/|___|  /____/__||__|  \___  >
+                       \/                   \/
+                                 no pwn no life
+
+docker-desktop# pwd
+/workhub/Desktop/ctfpwn-env
+docker-desktop#
+```
+Execute a command in the current directory, for example binwalk
+```
+# swing @ swingdeiMac in ~/Downloads/_a4bdea30bfcb8fbe6b1c159bf0a996d7_EG_RGOS11.9_6_B13P1T2,Release_09132119_.bin.extracted [0:53:39]
+$ msl --run binwalk 960.gz
+[check_container() - msl:93 ] Container status: running
+[main() - msl:139 ] Running command: bash -c "cd '/workhub/Downloads/_a4bdea30bfcb8fbe6b1c159bf0a996d7_EG_RGOS11.9_6_B13P1T2,Release_09132119_.bin.extracted' ; binwalk 960.gz"
+
+__________               .__  .__  _____
+\______   \__  _  ______ |  | |__|/ ____\____
+ |     ___/\ \/ \/ /    \|  | |  \   __\/ __ \
+ |    |     \     /   |  \  |_|  ||  | \  ___/
+ |____|      \/\_/|___|  /____/__||__|  \___  >
+                       \/                   \/
+                                 no pwn no life
+
+
+DECIMAL       HEXADECIMAL     DESCRIPTION
+--------------------------------------------------------------------------------
+0             0x0             gzip compressed data, maximum compression, from Unix, last modified: 1970-01-01 00:00:00 (null date)
+8357269       0x7F8595        Zip archive data, at least v2.0 to extract, compressed size: 1365, uncompressed size: 2795, name: keepon.html
+8358675       0x7F8B13        Zip archive data, at least v2.0 to extract, compressed size: 3489, uncompressed size: 9623, name: login.html
+8362204       0x7F98DC        Zip archive data, at least v2.0 to extract, compressed size: 6865, uncompressed size: 7919, name: login_image.jpg
+8430923       0x80A54B        Zip archive data, at least v2.0 to extract, compressed size: 20302, uncompressed size: 20373, name: style/logo.png
+8503960       0x81C298        Zip archive data, at least v2.0 to extract, compressed size: 4258, uncompressed size: 15874, name: online_mobile.htm
+8508265       0x81D369        Zip archive data, at least v2.0 to extract, compressed size: 2733, uncompressed size: 6624, name: offline.htm
+8511039       0x81DE3F        Zip archive data, at least v2.0 to extract, compressed size: 4524, uncompressed size: 15206, name: online.htm
+```
 ## build
 
 Store dockerfile for some versions of ubuntu.
@@ -47,4 +114,11 @@ provided tools
 - ROPGadgets
 - one_gadget
 - seccomp-tools
+...
+
+### pull
+
+```bash
+docker pulll beswing/swpwn:22.04
+```
 
---
+++ msl/msl.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env  python3
+
+# author : swing 
+# mail   : swing@mail.exp.sh
+
+import docker
+import os
+import platform
+import logging
+import argparse
+
+from distutils.dir_util import mkpath
+
+
+APPNAME = 'msl'
+APPAUTHOR = 'swing'
+
+SUPPORTED_UBUNTU_VERSION_LTS = [
+#    '14.04', Still many issues to be solved (version problems mostly)
+    '16.04',
+    '18.04',
+    '20.04',
+    '22.04',
+]
+
+client = docker.from_env()
+container = client.containers
+image = client.images
+
+class ColorWrite(object):
+    COLOR_SET = {
+            'END': '\033[0m',
+            'yellow': '\033[38;5;226m',
+            'red': '\033[31m',
+            'blue': '\033[34m',
+            'magenta': '\033[35m',
+            'cyan': '\033[36m',
+    }
+
+    @staticmethod
+    def color_write(content, color):
+        print(ColorWrite.COLOR_SET[color] + content + ColorWrite.COLOR_SET['END'])
+
+def colorwrite_init():
+    for color in ColorWrite.COLOR_SET:
+        # Use default value for lambda to avoid lazy capture of closure
+        setattr(ColorWrite, color, staticmethod(lambda x, color=color: ColorWrite.color_write(x, color)))
+
+# Static initialize ColorWrite
+colorwrite_init()
+
+def run_container(args):
+
+    if not args.ubuntu:
+        ubuntu = SUPPORTED_UBUNTU_VERSION_LTS[-1]
+    elif args.ubuntu in SUPPORTED_UBUNTU_VERSION_LTS:
+        ubuntu = args.ubuntu
+
+
+    if not args.directory:
+        args.directory = os.getenv('HOME')
+
+    if platform.system() == 'Darwin':
+        volumes = {
+            os.path.expanduser(args.directory) :{
+                'bind':'/workhub',
+                'mode':'rw'
+            }
+        }
+
+    privileged = True if args.priv else False
+    try:
+        running_container = container.run(
+            'beswing/swpwn:{}'.format(ubuntu),
+            '/bin/zsh',
+            cap_add=['SYS_ADMIN','SYS_PTRACE'],
+            security_opt=['seccomp:unconfined'],
+            detach=True,
+            tty=True,
+            volumes=volumes,
+            privileged=privileged,
+            network_mode = 'host',
+            name=args.name,
+            # remove=True,
+        )
+
+    except Exception as e:
+        logging.error('Failed to run : {}'.format(e))
+
+
+def check_container(args):
+    try:
+        container_status = container.get(args.name).status
+        if container_status != 'running':
+            container.get(args.name).start()
+        logging.info('Container status: {}'.format(container.get(args.name).status))
+        return True
+    except Exception as e:
+        logging.error('some error: {}'.format(e))
+        return False
+
+
+def commit_container(args):
+    try:
+        if check_container(args):
+            sub_container = container.get(args.name)
+            # old_image_sha = container.get(args.name).image.id.split(':')[1]
+            sub_container.commit(repository='beswing/swpwn', tag=args.ubuntu)
+            sub_container.stop()
+            # sub_container.remove(old_image_sha)
+
+    except Exception as e:
+        logging.error('some error: {}'.format(e))
+
+
+def remove_container(args):
+    try:
+        sub_container = container.get(args.name)
+        sub_container.stop()
+        sub_container.remove()
+    except Exception as e:
+        logging.error('some error: {}'.format(e))
+        return False
+
+
+def check_container_exists(args):
+    try:
+        sub_container = container.get(args.name)
+        return True
+    except Exception as e:
+        logging.error('some error: {}'.format(e))
+        return False
+
+def _attach_interactive(name,command):
+
+    cmd = "docker exec -it {} {}".format(
+            name,command
+        )
+    ColorWrite.yellow(
+        r'''
+__________               .__  .__  _____
+\______   \__  _  ______ |  | |__|/ ____\____
+ |     ___/\ \/ \/ /    \|  | |  \   __\/ __ \
+ |    |     \     /   |  \  |_|  ||  | \  ___/
+ |____|      \/\_/|___|  /____/__||__|  \___  >
+                       \/                   \/
+                                 no pwn no life
+'''
+    )
+    os.system(cmd)
+
+
+def run_command(args):
+
+    mslCmd = ' '.join(args.run)
+    command = 'bash -c "cd \'{}\' ; {}"'.format(PATH, mslCmd)
+    logging.info('Running command: {}'.format(command))
+    _attach_interactive(args.name, command)
+
+def main(args):
+
+    if args.commit == True:
+        commit_container(args)
+        exit()
+    elif args.priv == True or args.directory or args.restart:
+        remove_container(args)
+
+    if check_container(args) == False:
+        run_container(args)
+
+    run_command(args)
+
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='MacOS subsystem Linux power by Docker')
+    parser.add_argument('--ubuntu', default='22.04',type=str, help='choice ubuntu version')
+    parser.add_argument('--name', default='msl', type=str, help='container name')
+    parser.add_argument('--priv', action='store_true', help='privileged boot , so you can use something like kvm')
+    parser.add_argument('--dir', dest='directory',type=str, help='shared directory to be mounted at /workhub')
+    parser.add_argument('--run', default=['zsh'], dest='run', type=str, help='run command', nargs='+')
+    parser.add_argument('--commit',action='store_true', help='commit container to image')
+    parser.add_argument('--loglevel', default='INFO', type=str, help='log level')
+    parser.add_argument('--restart', action='store_true', default=False, help='restart')
+    args = parser.parse_args()
+    # run_container(args)
+    LOGFORMAT = "[%(funcName)s() - %(filename)s:%(lineno)s ] %(message)s"
+    logging.basicConfig(level=args.loglevel, format=LOGFORMAT)
+
+    main(args)
+
+if __name__ == '__main__':
+    PWD = os.getenv('PWD')
+    HOME = os.getenv('HOME')
+    PATH = PWD.replace(HOME, '/workhub')
+    parse_args()
